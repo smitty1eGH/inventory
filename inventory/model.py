31204e91a3f0917fd30dbe949e29b35bace3d765
chris smith <smitty1e@gmail.com>
2022-05-19 00:02:32 -0400

    Working on schema

--- inventory/model.py
+++ inventory/model.py
@@ -1,16 +1,22 @@
+import enum
 import logging
+import typing
 import uuid
 
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, DateTime, Enum, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
-# declarative base class
 Base = declarative_base()
-
 log = logging.getLogger(__name__)
 
-# CREATE TYPE user_level_enum        AS ENUM ('admin'  , 'standard')
-# CREATE TYPE order_item_status_enum AS ENUM ('ordered', 'received', 'cancelled')
+class UserLevelEnum(enum.Enum):
+    admin : str = 'admin'
+    standard : str = 'standard'
+
+class OrderItemStatusEnum(enum.Enum):
+    ordered : str = 'ordered'
+    received : str = 'received'
+    cancelled : str = 'cancelled'
 
 class Flags(Base):
     __tablename__ = "flags"
@@ -25,58 +31,61 @@ class Users(Base):
     __tablename__ = "users"
     user_id = Column(String, primary_key=True)
     user_email = Column(String)
-    user_level = Column(String)
+    user_level = Column(Enum(UserLevelEnum()),default=UserLevelEnum.standard)
+    children0 = relationship("Items")
+    children1 = relationship("Orders")
+    children2 = relationship("Sales")
 
 
-                CREATE TABLE IF NOT EXISTS items (
-                    item_id UUID PRIMARY KEY,
-                    user_id UUID REFERENCES users,
-                    item_name TEXT NOT NULL,
-                    item_category TEXT
-                )
+class Items(Base):
+    __tablename__ = "items"
+    item_id = Column(String, primary_key=True)
+    user_id = Column(String, ForeignKey('users.user_id'))
+    item_name = Column(String)
+    item_category = Column(String)
 
 
-                CREATE TABLE IF NOT EXISTS orders (
-                    order_id UUID PRIMARY KEY,
-                    user_id UUID REFERENCES users,
-                    order_created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-                    order_locked BOOLEAN NOT NULL DEFAULT FALSE,
-                    order_note TEXT DEFAULT ''
-                )
+class Orders(Base):
+    __tablename__ = "orders"
+    order_id = Column(String, primary_key=True)
+    user_id = Column(String, ForeignKey('users.user_id'))
+    order_created_at  = Column(DateTime)
+    order_locked = Column(Boolean, default=False)
+    order_note = Column(String)
 
 
-                CREATE TABLE IF NOT EXISTS sales (
-                    sale_id UUID PRIMARY KEY,
-                    user_id UUID REFERENCES users,
-                    sale_created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-                    sale_customer TEXT DEFAULT '',
-                    sale_paid BOOLEAN NOT NULL DEFAULT FALSE,
-                    sale_delivered BOOLEAN NOT NULL DEFAULT FALSE
-                )
+class Sales(Base):
+    __tablename__ = "sales"
+    sale_id = Column(String, primary_key=True)
+    user_id = Column(String, ForeignKey('users.user_id'))
+    sale_created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    sale_customer TEXT DEFAULT '',
+    sale_paid BOOLEAN NOT NULL DEFAULT FALSE,
+    sale_delivered BOOLEAN NOT NULL DEFAULT FALSE
 
 
-                CREATE TABLE IF NOT EXISTS order_items (
-                    order_id UUID REFERENCES orders ON DELETE CASCADE,
-                    item_id UUID REFERENCES items,
-                    quantity INT NOT NULL,
-                    status order_item_status_enum NOT NULL DEFAULT 'ordered',
-                    PRIMARY KEY (order_id, item_id)
-                )
+class OrderItems(Base):
+    __tablename__ = "order_items"
+    order_id UUID REFERENCES orders ON DELETE CASCADE,
+    item_id UUID REFERENCES items,
+    quantity INT NOT NULL,
+    status order_item_status_enum NOT NULL DEFAULT 'ordered',
+    PRIMARY KEY (order_id, item_id)
 
 
-                CREATE TABLE IF NOT EXISTS sale_items (
-                    sale_id UUID REFERENCES sales ON DELETE CASCADE,
-                    item_id UUID REFERENCES items,
-                    quantity INT NOT NULL,
-                    PRIMARY KEY (sale_id, item_id)
-                )
+class SaleItems(Base):
+    __tablename__ = "sale_items"
+    sale_id UUID REFERENCES sales ON DELETE CASCADE,
+    item_id UUID REFERENCES items,
+    quantity INT NOT NULL,
+    PRIMARY KEY (sale_id, item_id)
 
 
-                CREATE TABLE IF NOT EXISTS samples (
-                    sample_id UUID PRIMARY KEY,
-                    item_id UUID REFERENCES items,
-                    quantity INT NOT NULL,
-                    sample_used BOOLEAN NOT NULL DEFAULT FALSE
-                )
+class Samples(Base):
+    __tablename__ = "samples"
+    sample_id UUID PRIMARY KEY,
+    item_id UUID REFERENCES items,
+    quantity INT NOT NULL,
+    sample_used BOOLEAN NOT NULL DEFAULT FALSE
 
 
